be428edfa4bd28e08b0ae7fcdbf7eae09fa8df79
Ramu <yarramsettyramu1997@gmail.com>
2020-10-27 14:14:25 +0530

    snabdsnasmnd

---
+++ SystemTime.java
@@ -0,0 +1,11 @@
+package exercise;
+
+import java.util.Date;
+public class SystemTime {
+
+	public static void main(String args[])
+	{
+	    long start=System.currentTimeMillis();
+		System.out.println(new Date() +"\n");
+	}
+}
---
+++ TCmplxty.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class TCmplxty {
+	public static void main(String args[])
+	{
+		int count=11;
+		for(int i=1;i<=11;i++)
+		System.out.println("Time Complexity!!");
+	}
+
+}
